48f5d49fd501be0bf979251d01d158a150b0f570
Bjørn Rude Jacobsen <bjorn@breachvr.com>
2025-01-21 18:53:32 +0100

    Save progress

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-﻿import re
-import requests
-from bs4 import BeautifulSoup
+﻿import subprocess
 import pandas as pd
-import subprocess
-
+import requests
+import re
+from bs4 import BeautifulSoup
+from utils import getPrice, getAddress, getSize
 
 subprocess.run(['.venv\\Scripts\\activate.bat'], shell=True, check=True)
 
@@ -15,34 +15,20 @@ response.raise_for_status()  # Check if the request was successful
 # Step 2: Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(response.content, 'html.parser')
 
+# Save the HTML content to a file
 with open('output.html', 'w', encoding='utf-8') as file:
     file.write(soup.prettify())
 
-
-
-
-
 # Step 3: Extract the relevant data
-# properties = []
-# for listing in soup.find_all('article', class_='ads__unit'):
-#     title = listing.find('a', class_='ads__unit__link').get_text(strip=True)
-#     price = listing.find('div', class_='ads__unit__price').get_text(strip=True)
-#     location = listing.find('div', class_='ads__unit__location').get_text(strip=True)
-#     properties.append({'Title': title, 'Price': price, 'Location': location})
+properties = []
+price = getPrice(soup)
+address, area = getAddress(soup)
+size = getSize(soup)
+
+properties.append({'Prisantydning': price, 'Adresse': address, 'Postnummer': area, 'Størrelse': size})
 
 # Step 4: Store the extracted data in a pandas DataFrame
-# df = pd.DataFrame(properties)
+df = pd.DataFrame(properties)
 
 # Step 5: Save the DataFrame as a CSV file
-# df.to_csv('property_sales.csv', index=False)
-
-price_attr = {'aria-labelledby': 'sales-costs'}
-
-pricing_section = soup.find('section', price_attr)
-
-prisantydning_match = re.search(r'kr\s*([\d\s]+),-', pricing_section.get_text())
-if prisantydning_match:
-    prisantydning = prisantydning_match.group(1).replace('\xa0', ' ')
-    print(f'Prisantydning: {prisantydning}')
-else:
-    print('Prisantydning not found')
+df.to_csv('property_sales.csv', index=False)
---
+++ utils.py
@@ -0,0 +1,45 @@
+﻿import re
+
+
+def getPrice(soup):
+    price_attr = {'aria-labelledby': 'sales-costs'}
+    pricing_section = soup.find('section', price_attr)
+
+    prisantydning_match = re.search(r'kr\s*([\d\s]+),-', pricing_section.get_text())
+    prisantydning = None
+    if prisantydning_match:
+        prisantydning = prisantydning_match.group(1).replace('\xa0', '')
+    return prisantydning
+
+
+def getAddress(soup):
+    address_element = soup.find('span', {'data-testid': 'object-address'})
+    address = address_element.get_text().strip() if address_element else None
+    if address:
+        parts = address.split(',')
+        if len(parts) > 1:
+            address = parts[0].strip()
+            area = parts[1].strip()
+        else:
+            address = parts[0].strip()
+            area = None
+    else:
+        address = None
+        area = None
+    return address, area
+
+# def getSize(soup):
+#     bruksareal_element = soup.find('dd', text=re.compile(r'\d+\s*m²'))
+#     bruksareal = bruksareal_element.get_text().strip() if bruksareal_element else None
+#     if bruksareal:
+#         bruksareal_match = re.search(r'(\d+)\s*m²', bruksareal)
+#         bruksareal = bruksareal_match.group(1) if bruksareal_match else None
+#     return bruksareal
+
+def getSize(soup):
+    bruksareal_element = soup.find('div', {'data-testid': 'info-usable-area'}).find('dd', {'class': 'm-0 font-bold'})
+    bruksareal = bruksareal_element.get_text().strip() if bruksareal_element else None
+    if bruksareal:
+        bruksareal_match = re.search(r'(\d+)\s*m²', bruksareal)
+        bruksareal = bruksareal_match.group(1) if bruksareal_match else None
+    return bruksareal
