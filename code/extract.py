1878e0a73f1b442b6badb045cbd8997046aba2e4
Bjørn Rude Jacobsen <bjorn@breachvr.com>
2025-05-30 17:54:51 +0200

    Save ads to HTML

--- code/extract.py
+++ code/extract.py
@@ -3,27 +3,51 @@ import subprocess
 import time
 import pandas as pd
 from pandas import DataFrame
-
 from utils import *
 
 # Ensure the path to the virtual environment activation script is correct
 subprocess.run(['..\\.venv\\Scripts\\activate.bat'], shell=True, check=True)
 
 
-def extract_ad_data(url, index, name, saveToHTML=False):
+def get_ad_content(url, projectName, auto_save_new=True, force_save=False):
+    """
+    Fetches ad data from the given URL and saves the HTML content if specified.
+    :param url: The URL of the ad to extract data from.
+    :param projectName: The name of the project folder to save the HTML content.
+    :param force_save: If True, forces re-fetching of the ad data.
+    :return: A dictionary containing extracted ad data.
+    """
+    finnkode = url.split('finnkode=')[1]
+    html_file_path = f'{projectName}/html_extracted/{finnkode}.html'
+    exists = os.path.exists(html_file_path)
+
+    if (force_save):
+        print(f"Force-saving HTML content for {finnkode}.")
+        return save_ad_html_content(url, projectName, finnkode)
+
+    elif (exists or not auto_save_new):
+        print(f"HTML content for {finnkode} already exists. Skipping download.")
+        with open(html_file_path, 'r', encoding='utf-8') as file:
+            soup = BeautifulSoup(file.read(), 'html.parser')
+            return soup
+    else:
+        print(f"Saving HTML content for {finnkode}.")
+        return save_ad_html_content(url, projectName, finnkode)
+
+
+def save_ad_html_content(url, projectName, finnkode):
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.content, 'html.parser')
+    # Save the HTML content to a file inside the folder
+    html_file_path = f'{projectName}/html_extracted/{finnkode}.html'
+    with open(html_file_path, 'w', encoding='utf-8') as file:
+        file.write(str(soup))
+        return soup
 
-    # create the folder if it doesn't exist
-    os.makedirs(name, exist_ok=True)
-    # Create a folder inside the previous folder for the htmls
-    os.makedirs(f'{name}/html_extracted', exist_ok=True)
 
-    if (saveToHTML):
-        # Save the HTML content to a file inside the folder
-        with open(f'{name}/html_extracted/page{index}.html', 'w', encoding='utf-8') as file:
-            file.write(soup.prettify())
+def extract_ad_data(url, index, projectName):
+    soup = get_ad_content(url, projectName)
 
     address, area = getAddress(soup)
     sizes = getAllSizes(soup)
@@ -63,28 +87,28 @@ def extract_ad_data(url, index, name, saveToHTML=False):
     return data
 
 
-def extractDataFromAds(name: str, urls: DataFrame, outputFileName: str):
+def extractDataFromAds(projectName: str, urls: DataFrame, outputFileName: str):
     # Create the directory if it doesn't exist
-    os.makedirs(name, exist_ok=True)
+    os.makedirs(projectName, exist_ok=True)
 
     collectedData = []
 
     # Loop through each URL and extract property data
     try:
         # Create a folder inside the previous folder for the htmls
-        os.makedirs(f'{name}/html_extracted', exist_ok=True)
+        os.makedirs(f'{projectName}/html_extracted', exist_ok=True)
         for index, url in enumerate(urls['URL']):
             time.sleep(random.uniform(0.1, 0.1))
             try:
-                data = extract_ad_data(url, index, name)
+                data = extract_ad_data(url, index, projectName)
                 collectedData.append(data)
             except Exception as e:
                 print(f'Error processing URL at index {index}: {url} - {e}')
     finally:
         # Save the combined data to a new CSV file in the output directory
         df = pd.DataFrame(collectedData)
-        df.to_csv(f'{name}/{outputFileName}', index=False)
-        print(f"Data extraction completed. {len(collectedData)} records saved to {name}/{outputFileName}")
+        df.to_csv(f'{projectName}/{outputFileName}', index=False)
+        print(f"Data extraction completed. {len(collectedData)} records saved to {projectName}/{outputFileName}")
         return df
 
 
