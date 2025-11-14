e467e283d384c71c5ad757e122ef166a3240a9d8
Bjørn Rude Jacobsen <bjorn@breachvr.com>
2025-11-14 13:56:10 +0100

    Create parsers

--- main/debug.py
+++ main/debug.py
@@ -1,12 +1,11 @@
-﻿import requests
+﻿from bs4 import BeautifulSoup
 
-url = "https://www.finn.no/job/search?location=1.20001.20061&occupation=0.23"
-response = requests.get(url)
-response.raise_for_status()  # Raises an error if the request failed
+from main.parsing_helpers_jobs import getJobTitle, getOccupation, getJobPositions, getIndustry, getAdTitle, getDeadline
+from parsing_helpers_jobs import getCompany
 
+with open('jobbe/html_extracted/436826659.html', 'r', encoding='utf-8') as f:
+    html_content = f.read()
 
-
-with open("page.html", "w", encoding="utf-8") as f:
-    f.write(response.text)
-
-print("HTML downloaded and saved to page.html")
+soup = BeautifulSoup(html_content, 'html.parser')
+result = getDeadline(soup)
+print(f"Output: {result}")
